658f6f75ec897d0408485832fe94a04786da7f33
tepeliburak0 <80046774+tepeliburak0@users.noreply.github.com>
2021-11-26 16:02:06 +0300

    Core add Dtos and Services

---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByName(string userName);
+    }
+}
